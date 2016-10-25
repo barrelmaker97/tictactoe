cd46db64384449b5497ee9137916be30ecddc395
LuckierDodge <ryandlewis.rl@gmail.com>
2016-10-25 09:33:19 -0500

    Add prompt to replay at end of match

--- tic.cc
+++ tic.cc
@@ -8,124 +8,136 @@ string getFileContents(ifstream&);
 
 int main()
 {
-	srand(time(0));
-	Board board1;
-	int playerNum;
-	bool done = false;
-	system("clear");
-	displayArt("art.txt");
-	while (!done)
+	bool replay = true;
+	while(replay)
 	{
-		cout << "How many players? ";
-		cin >> playerNum;
-		if (playerNum == 0)
+		replay = false;
+		srand(time(0));
+		Board board1;
+		int playerNum;
+		bool done = false;
+		system("clear");
+		displayArt("art.txt");
+		while (!done)
 		{
-			char letter;
-			bool toggle = true;
-			while (!board1.isFull() and !board1.hasWon())
+			cout << "How many players? ";
+			cin >> playerNum;
+			if (playerNum == 0)
 			{
-				letter = (toggle)?'X':'O';
-				board1.turn(letter);
-				board1.print();
-				toggle = !toggle;
-			}
-			cout << "GAME OVER" << endl;
-			if (board1.hasWon())
-				cout << "Letter " << letter << " wins!" << endl;
-			else
-				cout << "Draw!" << endl;
-			done = true;
-		}
-		else if (playerNum == 1)
-		{
-			char choice;
-			char winner = 'O';
-			bool chosen = false;
-			cout << endl;
-			displayArt("example.txt");
-			while (!chosen)
-			{
-				cout << "Would you like to play as X or O? ";
-				cin >> choice;
-				if (choice == 'x' or choice == 'X')
+				char letter;
+				bool toggle = true;
+				while (!board1.isFull() and !board1.hasWon())
 				{
-					chosen = true;
-					system("clear");
-					displayArt("example.txt");
-					while (!board1.isFull() and !board1.hasWon())
+					letter = (toggle)?'X':'O';
+					board1.turn(letter);
+					board1.print();
+					toggle = !toggle;
+				}
+				cout << "GAME OVER" << endl;
+				if (board1.hasWon())
+					cout << "Letter " << letter << " wins!" << endl;
+				else
+					cout << "Draw!" << endl;
+				done = true;
+			}
+			else if (playerNum == 1)
+			{
+				char choice;
+				char winner = 'O';
+				bool chosen = false;
+				cout << endl;
+				displayArt("example.txt");
+				while (!chosen)
+				{
+					cout << "Would you like to play as X or O? ";
+					cin >> choice;
+					if (choice == 'x' or choice == 'X')
 					{
-						board1.input('X');
-						board1.print();
-						if (board1.isFull() or board1.hasWon())
+						chosen = true;
+						system("clear");
+						displayArt("example.txt");
+						while (!board1.isFull() and !board1.hasWon())
 						{
-							winner = 'X';
-							break;
+							board1.input('X');
+							board1.print();
+							if (board1.isFull() or board1.hasWon())
+							{
+								winner = 'X';
+								break;
+							}
+							board1.turn('O');
+							board1.print();
 						}
-						board1.turn('O');
-						board1.print();
+					}
+					else if (choice == 'o' or choice == 'O')
+					{
+						chosen = true;
+						system("clear");
+						displayArt("example.txt");
+						while (!board1.isFull() and !board1.hasWon())
+						{
+							board1.turn('X');
+							board1.print();
+							if (board1.isFull() or board1.hasWon())
+							{
+								winner = 'X';
+								break;
+							}
+							board1.input('O');
+							board1.print();
+						}
+					}
+					else
+					{
+						cout << "Please select a valid letter (X/O): " << endl;
+						cin.clear();
+						cin.ignore(1000, '\n');
 					}
 				}
-				else if (choice == 'o' or choice == 'O')
+				cout << "GAME OVER" << endl;
+				if (board1.hasWon())
 				{
-					chosen = true;
-					system("clear");
-					displayArt("example.txt");
-					while (!board1.isFull() and !board1.hasWon())
-					{
-						board1.turn('X');
-						board1.print();
-						if (board1.isFull() or board1.hasWon())
-						{
-							winner = 'X';
-							break;
-						}
-						board1.input('O');
-						board1.print();
-					}
+					cout << "Letter " << winner << " has won!" << endl;
 				}
 				else
+					cout << "Draw!" << endl;
+				done = true;
+			}
+			else if (playerNum == 2)
+			{
+				bool toggle = true;
+				char letter;
+				cout << endl;
+				system("clear");
+				displayArt("example.txt");
+				while (!board1.isFull() and !board1.hasWon())
 				{
-					cout << "Please select a valid letter (X/O): " << endl;
-					cin.clear();
-					cin.ignore(1000, '\n');
+					letter = (toggle)?'X':'O';
+					board1.input(letter);
+					board1.print();
+					toggle = !toggle;
 				}
-			}
-			cout << "GAME OVER" << endl;
-			if (board1.hasWon())
-			{
-				cout << "Letter " << winner << " has won!" << endl;
+				cout << "GAME OVER" << endl;
+				if (board1.hasWon())
+					cout << "Letter " << letter << " wins!" << endl;
+				else
+					cout << "Draw!" << endl;
+				done = true;
 			}
 			else
-				cout << "Draw!" << endl;
-			done = true;
-		}
-		else if (playerNum == 2)
-		{
-			bool toggle = true;
-			char letter;
-			cout << endl;
-			system("clear");
-			displayArt("example.txt");
-			while (!board1.isFull() and !board1.hasWon())
 			{
-				letter = (toggle)?'X':'O';
-				board1.input(letter);
-				board1.print();
-				toggle = !toggle;
+				cout << "Try again" << endl;
+				cin.clear();
+				cin.ignore(1000, '\n');
 			}
-			cout << "GAME OVER" << endl;
-			if (board1.hasWon())
-				cout << "Letter " << letter << " wins!" << endl;
-			else
-				cout << "Draw!" << endl;
-			done = true;
 		}
-		else
-		{
-			cout << "Try again" << endl;
-			cin.clear();
-			cin.ignore(1000, '\n');
-		}
+		
+		char decision;
+		cout << endl << "Would you like to play again? (Y for yes, any other key for no): ";
+		cin >> decision;
+
+		if (tolower(decision) == 'y')
+			replay = true;
 	}
 	return 0;
 }
